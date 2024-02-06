13c7304330cf926d8a11c6f0ce0a8d7d663bce47
Harisudan Sen <harisudan.sen@htcinc.com>
2024-02-06 17:07:27 +0530

    Adding boiler plate code Signed-off-by: Harisudan Sen <harisudan.sen@htcinc.com>

---
+++ src/main/java/com/udaipurvisit/udaipurvisit/UdaipurvisitApplication.java
@@ -0,0 +1,13 @@
+package com.udaipurvisit.udaipurvisit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdaipurvisitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdaipurvisitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udaipurvisit/udaipurvisit/UdaipurvisitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udaipurvisit.udaipurvisit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdaipurvisitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
